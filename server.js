beffd7de0046273ffbb3f1d4cfeb849dd495946e
Nonym0usse <cyril.vella@yahoo.com>
2022-04-30 13:42:13 +0200

    make it better

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ function requireHTTPS(req, res, next) {
   }
   next();
 }
+
 const express = require('express');
 const app = express();
+
 app.use(requireHTTPS);
+app.use(express.static('./dist/angular-heroku'));
+
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
+);
+
+app.listen(process.env.PORT || 8080);
